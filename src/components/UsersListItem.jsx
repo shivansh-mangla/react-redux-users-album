0de52e669db016ba2ddbc1113fa1267bbe560d63
shivansh mangla <shivanshmangla72@gmail.com>
2026-02-01 10:42:51 +0530

    Added album UI init

--- src/components/UsersListItem.jsx
+++ src/components/UsersListItem.jsx
@@ -3,6 +3,8 @@ import { GoTrashcan } from 'react-icons/go';
 import { useThunk } from '../hooks/useThunk';
 import { removeUser } from '../store';
 import Button from './Button';
+import ExpandablePanel from './ExpandablePanel';
+import AlbumsList from './AlbumsList';
 
 const UsersListItem = ({user}) => {
 
@@ -12,20 +14,21 @@ const UsersListItem = ({user}) => {
         doRemoveUser(user);
     }
 
-  return (
-    <div className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-            <div className="flex flex-row items-center justify-between">
-                <Button className="mr-3" loading={isLoading} onClick={handleClick}>
-                    <GoTrashcan />
-                </Button>
-
-                {error && <div>error deleting user </div>}
-                {user.name}
-            </div>
-        </div>
-    </div>
+    const header = (
+        <>
+        <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+            <GoTrashcan />
+        </Button>
+        {error && <div>Error deleting user.</div>}
+        {user.name}
+        </>
     );
+
+  return (
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpandablePanel>
+  );
 }
 
 export default UsersListItem
